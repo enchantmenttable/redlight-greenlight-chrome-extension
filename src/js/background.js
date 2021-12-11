f929a1c44010dfe085536b3b46df267cf69667b0
enchantmenttable <namson244@gmail.com>
2021-12-11 20:05:29 +0700

    prepare for structure change

--- src/js/background.js
+++ src/js/background.js
@@ -1,7 +1,46 @@
-chrome.runtime.onMessage.addListener((request) => {
-    if (request.message === "updateRedIcon") {
+async function readLocalStorage(key) {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(key, (data) => {
+            if (data[key] === undefined) {
+                reject("không thấy");
+            } else {
+                resolve(data[key])
+            }
+        })
+    })
+}
+
+chrome.runtime.onMessage.addListener(async (message, sender, response) => {
+    console.log("sender", sender);
+    console.log("sender tab", sender.tab);
+    console.log("sender tab id", sender.tab.id);
+    if (message.type === "updateRedIcon") {
         chrome.action.setIcon({ path: "../images/red-circle-32.png" });
-    } else if (request.message === "updateGreenIcon") {
+    } else if (message.type === "updateGreenIcon") {
         chrome.action.setIcon({ path: "../images/green-circle-32.png" });
+    } else if (message.type === "setInterval") {
+        chrome.storage.local.set({ intervalId: message.id }, data => console.log("setInterval", data));
+    } else if (message.type === "clearInterval") {
+        chrome.action.setIcon({ path: "../images/that-doll-32.png" });
+
+        // chrome.storage.local.get("intervalId", data => {
+        //     console.log("sender.tab.id", sender.tab.id);
+        //     chrome.tabs.sendMessage(sender.tab.id, { type: "clearInterval", id: data.intervalId }, data => console.log(data));
+        //     chrome.storage.local.remove("intervalId");
+        // })
+        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+            chrome.storage.local.get("intervalId", data => {
+                chrome.tabs.sendMessage(tabs[0].id, { type: "hehe", id: data.intervalId });
+                chrome.storage.local.remove("intervalId");
+            })
+        })
     }
+})
+
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.local.clear(() => { })
+})
+
+chrome.runtime.onStartup.addListener(() => {
+    chrome.storage.local.set({ play: false });
 })
--- src/js/popup.js
+++ src/js/popup.js
@@ -1,36 +1,86 @@
 const startButton = document.getElementById("start");
+const playButton = document.getElementById("play");
 
-startButton.addEventListener("click", async () => {
+async function readLocalStorage(key) {
+    return new Promise((resolve, reject) => {
+        chrome.storage.local.get(key, (data) => {
+            if (data[key] === undefined) {
+                reject("không thấy");
+            } else {
+                resolve(data[key])
+            }
+        })
+    })
+}
+
+// async function init() {
+//     playButton.checked = await readLocalStorage("play");
+// }
+// init()
+
+
+playButton.addEventListener("change", async () => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    console.log("done?");
     chrome.scripting.executeScript({
         target: { tabId: tab.id },
-        func: switchEvent,
+        args: [playButton.checked],
+        func: switchState,
     });
+
+    chrome.storage.local.set({ play: playButton.checked })
 })
 
-function switchEvent() {
+
+function switchState(isPlaying) {
     function redlight() {
         const allInputs = document.getElementsByTagName("input");
-        Array.from(allInputs).forEach(element => {
-            element.value = ""
+        Array.from(allInputs).forEach(input => {
+            input.value = ""
         });
     }
 
-    let switcher = 1;
-    setInterval(() => {
+    function play() {
         if (switcher) {
-            chrome.runtime.sendMessage({ message: "updateRedIcon" });
+            chrome.runtime.sendMessage({ type: "updateRedIcon" });
             setTimeout(() => {
                 document.addEventListener("keypress", redlight);
                 switcher = 0;
                 console.log("done red");
             }, 400);
         } else {
-            chrome.runtime.sendMessage({ message: "updateGreenIcon" });
+            chrome.runtime.sendMessage({ type: "updateGreenIcon" });
             document.removeEventListener("keypress", redlight);
             switcher = 1;
             console.log("done green");
         }
-    }, 3500);
+    }
+
+    let switcher = 1;
+    if (isPlaying) {
+        let intervalId = setInterval(play, 1500);
+        chrome.runtime.sendMessage({ type: "setInterval", id: intervalId });
+    } else {
+        chrome.runtime.sendMessage({ type: "clearInterval" });
+        console.log("unchecked");
+    }
+}
+
+chrome.runtime.onMessage.addListener((message, sender, response) => {
+    if (message.type === "hehe") {
+
+        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                args: [message.id],
+                func: injectClearInterval,
+            });
+
+        });
+
+    }
+})
+
+function injectClearInterval(intervalId) {
+    console.log("clearInterval from background received");
+    window.clearInterval(intervalId);
 }
