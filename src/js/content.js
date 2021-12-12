59091e9eb7f25feb2f046a3bbd943fe430c9d15c
enchantmenttable <namson244@gmail.com>
2021-12-12 12:07:42 +0700

    delete console.logs

--- src/js/content.js
+++ src/js/content.js
@@ -13,13 +13,11 @@ function switcher() {
         setTimeout(() => {
             document.addEventListener("keypress", redlight);
             state = 0;
-            console.log("done red");
         }, 400);
     } else {
         chrome.runtime.sendMessage({ type: "updateGreenIcon" });
         document.removeEventListener("keypress", redlight);
         state = 1;
-        console.log("done green");
     }
 }
 
@@ -27,13 +25,9 @@ currentIntervalId = 0;
 
 chrome.runtime.onMessage.addListener((message, sender, response) => {
     if (message.type === "stop") {
-        console.log("stop msg received");
         window.clearInterval(currentIntervalId);
-        console.log(currentIntervalId);
         chrome.runtime.sendMessage({ type: "stop" });
     } else if (message.type === "play") {
-        console.log("play msg received");
         currentIntervalId = setInterval(switcher, 4000);
-        console.log("interval set", currentIntervalId);
     }
 })
